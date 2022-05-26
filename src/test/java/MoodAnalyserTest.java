b19ff612518868cdddf9837cf7f8d1cd29d2382f
Rohit Singh Mehra <rohit264cool@gmail.com>
2022-05-26 07:39:41 +0530

    Analyse the given mood would be either sad or else happy

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -4,9 +4,16 @@ import org.junit.jupiter.api.Test;
 public class MoodAnalyserTest {
 
     @Test
-    void givenMessage_shouldReturnSad() {
-            MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = MoodAnalyser.analyseMood("I am in sad mood");
+    void givenMessage_analyseSadMood() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in sad mood");
         Assertions.assertEquals("SAD", mood);
+    }
+
+    @Test
+    void givenMessage_AnalyseHappyMood() {
+        MoodAnalyser moodAnalyzer = new MoodAnalyser();
+        String mood = moodAnalyzer.analyseMood("I am in any mood");
+        Assertions.assertEquals("HAPPY", mood);
     }
 }
